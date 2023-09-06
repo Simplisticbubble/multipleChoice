bd61d1e653c671c40e665d1a13b4b728c37d3a78
Simplisticbubble <ks.kevinseu@gmail.com>
2023-09-06 16:29:46 +1000

    multipleChoice finished

---
+++ GITHUB/multipleChoice/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+import './App.css'
+
+export default function App(){
+  const questions = [
+    {
+      question: "What is the capital of France?",
+      options: ['London', 'Paris', 'Berlin', 'Madrid'],
+      correctAnswer: 'Paris',
+    },
+    {
+        question: 'Which planet is known as the Red Planet?',
+        options: ['Mars', 'Venus', 'Jupiter', 'Saturn'],
+        correctAnswer: 'Mars',            
+    },
+  ];
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const handleAnswer = (selectedOption) => {
+    const currentQuestion = questions[currentQuestionIndex];
+    if (selectedOption === currentQuestion.correctAnswer){
+        setScore((prevScore) => prevScore +1);
+    }
+    if (currentQuestionIndex +1 < questions.length){
+      setCurrentQuestionIndex((prevQuestionIndex)=>prevQuestionIndex+1);
+    }
+  }
+  const showScoreAlert = ()=>{
+    alert(`Your score: ${score}/${questions.length}`);
+  }
+  return(
+    <>
+    <h1>Quiz</h1>
+      <div>
+        <h3>{questions[currentQuestionIndex].question}</h3>
+        <ul>
+          {questions[currentQuestionIndex].options.map((option, index) => (
+              <li key = {index}>
+                <button onClick={() => handleAnswer(option)}>{option}</button>
+              </li>
+          )
+          )}
+        </ul>
+        <button onClick = {() => showScoreAlert()}>Show score</button>
+      </div>
+    </>
+  );
+}
+
